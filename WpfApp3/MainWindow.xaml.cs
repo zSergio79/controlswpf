20d50c18797e157b9f4a67a27a1a0b4cc683a777
Sergey Kosinov <sergio1979@list.ru>
2020-12-21 21:02:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseMove(object sender, MouseEventArgs e)
+        {
+            var btn = (Panel)sender;
+            var gradient = btn.OpacityMask as RadialGradientBrush;
+            if (gradient == null)
+                return;
+            Point pt = Mouse.GetPosition(btn);
+            gradient.GradientOrigin = new Point(pt.X / btn.ActualWidth, pt.Y / btn.ActualHeight);
+            gradient.RadiusX = 0.25;
+            gradient.RadiusY = 0.75;
+            gradient.Center = gradient.GradientOrigin;
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show("Click");
+            lbl.Content = (sender as Button).Content;
+        }
+
+        private void Button_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            lbl.Content = "2";
+        }
+
+        private void UniformGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var btn = (Panel)sender;
+            var gradient = btn.OpacityMask as RadialGradientBrush;
+            if (gradient == null)
+                return;
+            gradient.GradientOrigin = new Point(-1,-1);
+            gradient.Center = gradient.GradientOrigin;
+        }
+    }
+}
